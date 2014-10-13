3ca90d7e82e32d55605cc0410aaf6dd8e4dced6e
Paul Robson <paul@robsons.org.uk>
2014-10-13 09:10:14 +0100

    Collision manager does not access private members

--- source/game/collisions.lua
+++ source/game/collisions.lua
@@ -11,8 +11,6 @@
 --- ************************************************************************************************************************************************************************
 --																	Collisions Manager Object
 --- ************************************************************************************************************************************************************************
---										 	THIS ACCESSES CAROUSEL MEMBERS DIRECTLY FOR SPEED (m_x,m_y,m_radius)
---- ************************************************************************************************************************************************************************
 
 local CollisionManager = Framework:createClass("game.collision.checker")
 
@@ -29,16 +27,22 @@ function CollisionManager:getCollisions(gap)
 	local collisionList = {} 														-- list of collisions.
 	if carouselCount <= 1 then return collisionList end 							-- if less than 2 there can't possibly be any collisions !
 	local objectArray = {}
-	for _,obj in pairs(carouselTable) do objectArray[#objectArray+1] = obj end 		-- convert from a hash to an array as it is quicker.
+	local objectData = {}
+	for _,obj in pairs(carouselTable) do 											-- convert from a hash to an array as it is quicker.
+		objectArray[#objectArray+1] = obj 
+		objectData[#objectData+1] = obj:getStatus() 								-- pick up the object data at the same time.
+	end 		
 	for testIndex = 1,#objectArray-1 do 											-- for each in the table, except the last one
 		local obj1 = objectArray[testIndex]
+		local obd1 = objectData[testIndex]
 		for altIndex = testIndex+1,#objectArray do  								-- compare against every other one.
 			local obj2 = objectArray[altIndex]
-			local minDist = obj1.m_radius + obj2.m_radius + gap						-- the minimum distance between centres for a collision
-			if math.abs(obj1.m_x-obj2.m_x) <= minDist then 							-- check if they *might* collide, intersecting boxes first as its quicker.
-				if math.abs(obj1.m_y-obj2.m_y) <= minDist then 
-					local dx = obj1.m_x - obj2.m_x 									-- now do the proper calculation. 
-					local dy = obj1.m_y - obj2.m_y
+			local obd2 = objectData[altIndex]
+			local minDist = obd1.radius + obd2.radius + gap							-- the minimum distance between centres for a collision
+			if math.abs(obd1.x-obd2.x) <= minDist then 								-- check if they *might* collide, intersecting boxes first as its quicker.
+				if math.abs(obd1.y-obd2.y) <= minDist then 
+					local dx = obd1.x - obd2.x 										-- now do the proper calculation. 
+					local dy = obd1.y - obd2.y
 					if dx * dx + dy * dy < minDist * minDist then 					-- if it is actually in range.
 						collisionList[#collisionList+1] = { obj1, obj2 } 			-- add to the collision list.
 						--print(#collisionList,obj1.m_identifier,obj2.m_identifier)
